a57face2feb8b25f1f4e8e772dce4d32fa29d0f6
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-17 02:32:30 +0300

    setCurrentTime-second-try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,17 +9,18 @@ function onPlay({ seconds }) {
      localStorage.setItem("videoplayer-current-time", JSON.stringify({seconds}))
 }
 const LS = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(LS);
 
-player.setCurrentTime(LS).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+//     .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
