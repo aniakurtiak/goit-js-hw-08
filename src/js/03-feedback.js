f7f0e1b8ef2e8ff6c835a7e9b27074ebf81372e8
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-18 02:20:45 +0300

    task3data

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,15 @@
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', handlerInput);
-const data = {};
+// const data = {};
 function handlerSubmit(evt) {
     evt.preventDafault();
     const { email, message } = evt.currentTarget.elements;
-    data[evt.currentTarget.name] = evt.target.value;
+    // data[evt.currentTarget.name] = evt.target.value;
+   const data = {
+        em: email.value,
+        text: message.value
+    }
+    console.log(data);
 }
 
 form.addEventListener('submit', handlerSubmit);
