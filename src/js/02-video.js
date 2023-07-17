645e5de80445816ad83401969599f34686fa214f
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-18 02:05:41 +0300

    task3-input

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,19 +8,17 @@ const iframe = document.querySelector('iframe');
 function onPlay({ seconds }) {
      localStorage.setItem("videoplayer-current-time", JSON.stringify({seconds}))
 }
-const LS = localStorage.getItem("videoplayer-current-time");
-player.setCurrentTime(LS);
+const LS = localStorage.getItem("videoplayer-current-time", );
+player.setCurrentTime(LS).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
-//     .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', handlerInput);
+const data = {};
+function handlerSubmit(evt) {
+    evt.preventDafault();
+    const { email, message } = evt.currentTarget.elements;
+    data[evt.currentTarget.name] = evt.target.value;
+}
 
+form.addEventListener('submit', handlerSubmit);
+
+const dataInput = localStorage.setItem("feedback-form-state", JSON.stringify(data));
+console.log(dataInput);
