ed8e404ec51fb7dd4965af5d3aec24b93af176f5
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-18 23:26:33 +0000

    Deploying to gh-pages from @ aniakurtiak/goit-js-hw-08@c083c127e28ccb7511a4df9c8ae37bbf18d5569d 🚀

--- 03-feedback.d3d3e2d2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){a[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(a))}));var a={};var t=JSON.parse(localStorage.getItem("feedback-form-state"));console.log(t);t&&(e.querySelector('input[name="email"]').value=t.email),t&&(e.querySelector('textarea[name="message"]').value=t.email)}();
-//# sourceMappingURL=03-feedback.d3d3e2d2.js.map
