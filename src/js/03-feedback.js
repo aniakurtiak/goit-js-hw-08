ebc8ba8c6c6973c4a76466914efb7a220e39581c
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-19 04:04:17 +0300

    task3-04

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,7 @@
+import { throttle } from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', handlerInput);
+form.addEventListener('input', throttle(handlerInput, 500));
 form.addEventListener('submit', handlerSubmit);
 
 const data = {};
@@ -12,7 +14,7 @@ function saveFormState() {
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
 };
 
-const LS = JSON.parse(localStorage.getItem('feedback-form-state'));
+const LS = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 const formInput = form.querySelector('input[name="email"]');
 const formText = form.querySelector('textarea[name="message"]');
 
@@ -26,7 +28,7 @@ fillForm();
 function handlerSubmit(evt) {
   evt.preventDefault();
   localStorage.removeItem('feedback-form-state');
+  console.log(LS);
   formInput.value = '';
   formText.value = '';
-  console.log(LS);
 }
